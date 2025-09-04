5ed0cd504c3b36ef086c2094d29069337bec6595
njoroge <ellyjohnmwangi@gmail.com>
2025-09-04 13:23:12 +0300

    API endpoints upload, view created

--- src/main/java/com/mwas/photoz/PhotozController.java
+++ src/main/java/com/mwas/photoz/PhotozController.java
@@ -1,12 +1,50 @@
 package com.mwas.photoz;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @RestController
 public class PhotozController {
+
+    private Map<String, Photo> db = new HashMap<>(){{
+        put("1", new Photo("1", "test.jpg"));
+    }};
     @GetMapping
     public String hello() {
         return "Hello from Spring Boot!";
+    }
+
+    @GetMapping("/photos")
+    public Collection<Photo> getPhotos() {
+        return db.values();
+    }
+    @GetMapping("/photos/{id}")
+    public Photo get(@PathVariable String id) {
+        Photo photo = db.get(id);
+        if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return photo;
+    }
+    @DeleteMapping("/photos/{id}")
+    public void delete(@PathVariable String id) {
+        Photo photo = db.remove(id);
+        if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+    @PostMapping("/photos")
+    public Photo create(@RequestPart("data") MultipartFile file) throws IOException {
+        Photo photo = new Photo();
+        photo.setId(UUID.randomUUID().toString());
+        photo.setFileName(file.getOriginalFilename());
+        photo.setData(file.getBytes());
+        db.put(photo.getId(), photo);
+        return photo;
     }
 }
